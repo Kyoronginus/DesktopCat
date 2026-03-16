6c0cd2cd71537d1498b8818199c2be21bdd5850c
Tohru <tohrudj0711@gmail.com>
2026-03-16 10:24:57 +0700

    Initial Commit

---
+++ DesktopCat/DesktopCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopCatApp.swift
+//  DesktopCat
+//
+//  Created by Tohru Djunaedi Sato on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DesktopCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
